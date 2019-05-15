8a823b9384d657cd64d4784f3ed1f86ecd6d8a73
clizardo <clizardo@svi.com.ph>
2019-05-15 15:04:31 +0800

    added radio buttons

--- binder/widget_binder.js
+++ binder/widget_binder.js
@@ -24,18 +24,23 @@ function setValues() {
 
 function setHandlers() {
     $(widgets.PaymentFormWidget.getForm()).submit(function() {
-        var data = decodeURIComponent($(widgets.PaymentFormWidget.getForm()).serialize()).replace(/&/g, ',');
-        var secretKey = '8c4b9d0291964771b5f28fd40ace5674cde8e6f2c15e48efb07dd716d4fba60cc90157f1af4149458d028ae906a79d0ef8fbda4c258e4f3f80bc78f0b7f00d294732c48a7aee4603826e66112e25c65ac3670b6955c14bf59f7e459d3b9e7a94d05a4ffb2d1448e5b79d0ff53a149d878db590f508ff46e3815aa6b172534e80';
-        var hash = CryptoJS.HmacSHA256(data, secretKey);
-        var base64 = CryptoJS.enc.Base64.stringify(hash);
-        $('<input>').attr({
-            type: 'hidden',
-            id: 'signature',
-            name: 'signature',
-            value: base64
-        }).appendTo(widgets.PaymentFormWidget.getForm());
-
-        return true;
+        if($('input[name=paymentMode]:checked').val() == "Credit Card") {
+            $("input[type=radio]").prop('disabled', true);
+            var data = decodeURIComponent($(widgets.PaymentFormWidget.getForm()).serialize()).replace(/&/g, ',');
+            var secretKey = '8c4b9d0291964771b5f28fd40ace5674cde8e6f2c15e48efb07dd716d4fba60cc90157f1af4149458d028ae906a79d0ef8fbda4c258e4f3f80bc78f0b7f00d294732c48a7aee4603826e66112e25c65ac3670b6955c14bf59f7e459d3b9e7a94d05a4ffb2d1448e5b79d0ff53a149d878db590f508ff46e3815aa6b172534e80';
+            var hash = CryptoJS.HmacSHA256(data, secretKey);
+            var base64 = CryptoJS.enc.Base64.stringify(hash);
+            $('<input>').attr({
+                type: 'hidden',
+                id: 'signature',
+                name: 'signature',
+                value: base64
+            }).appendTo(widgets.PaymentFormWidget.getForm());
+            return true;
+        } else if ($('input[name=paymentMode]:checked').val() == "Invoice") {
+            alert("Invoice picked");
+            return false;
+        }
     });
 }
 
--- widgets/PaymentForm.js
+++ widgets/PaymentForm.js
@@ -11,6 +11,7 @@ var PaymentForm = function()  {
     function initialize() {
         var hiddenFields = generateHiddenFields();
         var visibleFields = generateVisibleFields();
+        var radios = generateRadioButtons();
         
         var submitBtn = global.submitBtn = document.createElement("input");
         $(submitBtn).attr({
@@ -30,6 +31,7 @@ var PaymentForm = function()  {
         });
         $(form).append(hiddenFields);
         $(form).append(visibleFields);
+        $(form).append(radios);
         $(form).append(submitBtn);
 
         var wrapper = global.wrapper = document.createElement("div");
@@ -69,7 +71,7 @@ var PaymentForm = function()  {
                 name: VISIBLE_FIELDS[x].name,
                 readonly: VISIBLE_FIELDS[x].readonly,
             });
-            $(label).addClass("fieldInput");
+            $(field).addClass("fieldInput");
 
             var fieldContainer = document.createElement("div");
             $(fieldContainer).append(label);
@@ -77,71 +79,16 @@ var PaymentForm = function()  {
             $(fieldContainer).addClass("fieldContainer");
 
             $(wrapper).append(fieldContainer);
-
         }
 
         return wrapper;
     }
 
-    function generateFields() {
-
-        var transactionTypeLabel = document.createElement("label");
-        $(transactionTypeLabel).text("Transaction Type: ");
-        $(transactionTypeLabel).addClass("transactionTypeLabel");
-
-        var transactionType = global.transaction_type = document.createElement("select");
-        generateOptions(transactionType, "Transaction type");
-        $(transactionType).addClass("transactionType");
-
-        var transactionTypeContainer = document.createElement("div");
-        $(transactionTypeContainer).append(transactionTypeLabel);
-        $(transactionTypeContainer).append(transactionType);
-        $(transactionTypeContainer).addClass("fieldContainer");
-
-        var referenceNumberLabel = document.createElement("label");
-        $(referenceNumberLabel).text("Reference Number: ");
-        $(referenceNumberLabel).addClass("referenceNumberLabel");
-
-        var referenceNumber = global.reference_number = document.createElement("label");
-        $(referenceNumber).addClass("transactionType");
-
-        var referenceNumberContainer = document.createElement("div");
-        $(referenceNumberContainer).append(referenceNumberLabel);
-        $(referenceNumberContainer).append(referenceNumber);
-        $(referenceNumberContainer).addClass("fieldContainer");
-
-        var amountLabel = document.createElement("label");
-        $(amountLabel).text("Amount: ");
-        $(amountLabel).addClass("amountLabel");
-
-        var amount = global.amount = document.createElement("label");
-        $(amount).addClass("transactionType");
-
-        var amountContainer = document.createElement("div");
-        $(amountContainer).append(amountLabel);
-        $(amountContainer).append(amount);
-        $(amountContainer).addClass("fieldContainer");
-
-        var currencyLabel = document.createElement("label");
-        $(currencyLabel).text("Currency: ");
-        $(currencyLabel).addClass("currencyLabel");
-
-        var currency = global.currency = document.createElement("label");
-        $(currency).addClass("transactionType");
-
-        var currencyContainer = document.createElement("div");
-        $(currencyContainer).append(currencyLabel);
-        $(currencyContainer).append(currency);
-        $(currencyContainer).addClass("fieldContainer");
-
+    function generateRadioButtons() {
         var radioCC = generateRadio("Credit Card");
         var radioInvoice = generateRadio("Invoice");
 
         var wrapper = document.createElement("div");
-        $(wrapper).append(transactionTypeContainer);
-        $(wrapper).append(referenceNumberContainer);
-        $(wrapper).append(amountContainer);
-        $(wrapper).append(currencyContainer);
         $(wrapper).append(radioCC);
         $(wrapper).append(radioInvoice);
         $(wrapper).addClass("visibleFields");
@@ -166,57 +113,8 @@ var PaymentForm = function()  {
         $(wrapper).append(radio);
         $(wrapper).append(radioLabel);
         $(wrapper).addClass("radioContainer");
+
         return wrapper;
-    }
-
-    function generateOptions(select, placeholderText) {
-        var placeholder = document.createElement("option");
-        $(placeholder).attr({
-            disabled:'disabled',
-            selected:'selected',
-            hidden:'hidden'
-        });
-        $(placeholder).text(placeholderText);
-        $(placeholder).val("");
-
-        var authorizationOptn = document.createElement("option");
-        $(authorizationOptn).text("Authorization");
-        $(authorizationOptn).val("authorization");
-
-        var saleOptn = document.createElement("option");
-        $(saleOptn).text("Sale");
-        $(saleOptn).val("sale");
-
-        $(select).append(placeholder);
-        $(select).append(authorizationOptn);
-        $(select).append(saleOptn);
-    }
-
-    function constructor() {
-        data.access_key = "77246056409d34449977303b767c984c";
-        data.profile_id = "60DC420E-7BF0-4BAE-8C43-4F30C5F153C5";
-        data.transaction_uuid = generateUniqid();
-        data.signed_field_names = "access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency";
-        data.unsigned_field_names = "";
-        data.signed_date_time = new Date().toISOString().split(".").shift() + "Z";
-        data.locale = "en";
-    }
-
-    function generateUniqid() {
-        var a = "";
-        var b = false;
-        var c = Date.now()/1000;
-        var d = c.toString(16).split(".").join("");
-        while(d.length < 14){
-            d += "0";
-        }
-        var e = "";
-        if(b){
-            e = ".";
-            var f = Math.round(Math.random()*100000000);
-            e += f;
-        }
-        return a + d + e;
     }
 
     PaymentForm.prototype.getSubmitButton = function() { 
